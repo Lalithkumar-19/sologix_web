0f782c5cd239afcba60b824a2af9e4a212cf0fe4
lalith <lalithdev123@gmail.com>
2025-07-20 00:16:29 +0530

    porrt

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { getUserCart, addToCart,removefromCart } = require("../controllers/CartControllers");
+const { VerifyToken } = require("../middlewares");
 const router = express.Router();
 
 
 router.get("/Get-user-cart", getUserCart);
 router.post("/Add-to-cart",addToCart);
-router.delete('/Remove-from-cart', authMiddleware, removefromCart);
+router.delete('/Remove-from-cart',VerifyToken , removefromCart);
 
 module.exports = router;
