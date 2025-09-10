358c6f0462487ae5a3c9c4186cb655315f5565fb
lalith <lalithdev123@gmail.com>
2025-09-11 00:43:51 +0530

    base contetoller

--- src/controllers/BaseController.js
+++ src/controllers/BaseController.js
@@ -223,7 +223,7 @@ const ValidateAlphanumeric = (text) => {
   return re.test(String(text));
 };
 
-const ValidateLength = (text, min = 1, max = 25) => {
+const ValidateLength = (text, min = 2, max = 25) => {
   return text.length >= min && text.length <= max ? true : false;
 };
 
