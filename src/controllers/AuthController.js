113e781ac1d4e35273ed83e59ed4eea59f8f7b93
lalith <lalithdev123@gmail.com>
2025-09-11 01:05:22 +0530

    base contetoller 2

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -145,7 +145,7 @@ module.exports = {
   },
   ContactUs: async (req, res, next) => {
     try {
-      const { name, phone, email, state, city, subject, message, interest } =
+      const { name, phone, email, state, city, subject, message, intrest} =
         req.body;
       if (!ValidateAlphanumeric(name.trim()))
         return HandleError(res, "Please enter a valid name!");
@@ -166,7 +166,7 @@ module.exports = {
 
       const createContact = await Insert({
         model: Contact,
-        data: { name, phone, email, state, city, subject, message, intrest: interest },
+        data: { name, phone, email, state, city, subject, message, intrest },
       });
       if (!createContact) {
         return HandleError(res, "Failed to send message!");
